3cb3935d85ea7e137dda421a8e5a5fcece8be481
asherlie <lieber.asher@gmail.com>
2018-04-17 03:12:15 -0400

    removed unused code

--- c/email_notifier.c
+++ c/email_notifier.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
-/*#include <stdbool.h>*/
-/*#include <stdlib.h>*/
 #include <curl/curl.h>
-/*#include <fstream>*/
 #include <string.h>
 #include "email_notifier.h"
-//#include <sstream>
 
-/*
- *#ifdef LIBB64
- *#include "base64.h"
- *#endif
- */
-
-//#define LIBB64
+// TODO: fix ridiculous memory leaks
 
 char auth_filename[] = "auth.txt";
 
@@ -54,77 +44,37 @@ char** get_auth_info(){
 	return ret;
 }
 
-//std::ifstream::pos_type filesize(std::string fn){ //returns filesize
-//int filesize(std::string fn){ //returns filesize -- why use ifs::pos_type
 int filesize(char* fn){
       FILE* fp = fopen(fn, "r");
       fseek(fp, 0L, SEEK_END);
       size_t sz = ftell(fp);
       fclose(fp);
       return sz/1000000;
-	//return inf.tellg()/1000000;
 }
 
 char* build_MIME(char* subject, char* message, char** attachments, int n_attachments){
       char* contents = calloc(sizeof(char), 10000);
-
       sprintf(contents, "Content-Type: multipart/mixed; boundary=adkkibiowiejdkjbazZDJKOIe\nSubject: %s\n--adkkibiowiejdkjbazZDJKOIe\nContent-Type: multipart/alternative; boundary=adkkibiowiejdkjbazZDJKOIe1\n--adkkibiowiejdkjbazZDJKOIe1\n Content-Type: text/plain; charset=UTF-8\n\n%s\n--adkkibiowiejdkjbazZDJKOIe1--\n" , subject, message);
-/*
- *      strcat(contents, "Content-Type: multipart/mixed; boundary=adkkibiowiejdkjbazZDJKOIe\n"); //adkkibiowiejdkjbazZDJKOIe is an arbitrary boundary -
- *      contents += "Subject: " + subject + "\n";									    // - something that won't come up in a message or subject
- *      sprintf
- *      contents += "--adkkibiowiejdkjbazZDJKOIe\nContent-Type: multipart/alternative; boundary=adkkibiowiejdkjbazZDJKOIe1\n";
- *
- *      contents += "--adkkibiowiejdkjbazZDJKOIe1\nContent-Type: text/plain; charset=UTF-8\n\n" + message +"\n--adkkibiowiejdkjbazZDJKOIe1--\n";
- */
-
-	//char* cmd;
 	char* b64_file;
-
       char* p_text_file;
       char* tmp;
 	//adding attachments encoded in base64 to MIME format
 	for(int i = 0; i < n_attachments; ++i){ 
-/*
- *            #ifdef LIBB64
- *            //std::ifstream ifs(attachments[i]);
- *            FILE* fp = fopen(attachments[i], "r");
- *            std::stringstream ss;
- *            ss << ifs.rdbuf();
- *            p_text_file = ss.str();
- *
- *            Base64::Encode(p_text_file, &b64_file);
- *            contents += b64_file;
- *            p_text_file = ""; b64_file = "";
- *            //#endif // LIBB64
- *            #else
- */
-            /*printf("checking attachments[%i]: %s\n", i, attachments[i]);*/
-		/*if(strcmp(attachments[i], "") == 0)break; //TODO find a better place to brk*/
-            if(strlen(attachments[i]) == 0 || attachments[i][0] == '\0' || attachments[i][0] == '\n')break;
-		//if(attachments[i] == "" || i >= sizeof(attachments)/sizeof(std::string))break; //TODO find a better place to brk
+            // TODO: implement base64 library method of MIME encoding
+            // TODO: use ifdef to detect LIB_B64 macro
             char tmp_cont[4096];
             sprintf(tmp_cont, "\n--adkkibiowiejdkjbazZDJKOIe\nContent-Type: application/octet-stream; name=\"%s\"\nContent-Transfer-Encoding: Base64\nContent-Disposition: attachment; filename=\"%s\"\n", attachments[i], attachments[i]);
             strcat(contents, tmp_cont);
-		//contents += "\n--adkkibiowiejdkjbazZDJKOIe\n";
-		//contents += "Content-Type: application/octet-stream; name=\"" + attachments[i] + "\"\nContent-Transfer-Encoding: Base64\nContent-Disposition: attachment; filename=\"" + attachments[i] +"\"\n";
             char cmd[100];
             sprintf(cmd, "cat \"%s\" | base64 | cat > .tmp_b64_file_attachment", attachments[i]);
-            //cmd = "cat \"" + attachments[i] + "\"| base64 | cat > .tmp_b64_file_attachment";
             system(cmd);
-            //cmd = "";
-            //std::ifstream in_s(".tmp_b64_file_attachment");
             FILE* fp = fopen(".tmp_b64_file_attachment", "r");
             size_t sz = 0;
             while(getline(&b64_file, &sz, fp) != EOF){
                   strcat(contents, b64_file);
-                  //contents += b64_file;
             }
-            //b64_file = "";
             system("rm .tmp_b64_file_attachment");
-            //#endif
 	}
-	//contents += "\n--adkkibiowiejdkjbazZDJKOIe--\n";
       strcat(contents, "\n--adkkibiowiejdkjbazZDJKOIe--\n");
 	return contents;
 }
@@ -139,30 +89,22 @@ int notify(struct notification_message* nm){
 			if(tmp_size + size_so_far <= MAX_ATTACHMENT_SIZE){ //if i can spare some MB's 
 				size_so_far += tmp_size; 
                         good_attachments[c++] = nm->attachments[i];
-				/*good_attachments[c++] = nm->attachments[i];*/
 			}
 			else {
-                        strcat(nm->message, "FILE TOO BIG\n");
-				//nm->message += ("\nA file you attempted to attach is too large. It is " + std::to_string(tmp_size) + "MB's. It can be found here: \"" + nm->attachments[i] + "\" on the server\n");
+                        // TODO: add more meaningful output, print max size and size of file for example
+                        strcat(nm->message, "FILE TOO LARGE\n");
 			}
 		}
 		else {
-			//nm->message += ("\nThe file: '" + nm->attachments[i] + "' that you tried to attach does not exist\n");
+                  // TODO: add more meaningful output
                   strcat(nm->message, "FILE DOES NOT EXIST\n");
 		}
 		tmp_size = 0;
 	}
-      good_attachments[c] = malloc(sizeof(char));
-      good_attachments[c] = '\0';
-	
-
       char tmp_file[] = ".tmp_mail_contents";
-	//std::ofstream ostr;
       FILE* fp = fopen(tmp_file, "w");
-	//ostr.open(tmp_file);
       fputs(build_MIME(nm->subject, nm->message, good_attachments, c), fp);
 
-	//ostr.close();
       fclose(fp);
 	CURL* curl;
 	FILE* FP;
@@ -174,28 +116,19 @@ int notify(struct notification_message* nm){
 		FP = fopen(tmp_file, "r");
 		curl_easy_setopt(curl, CURLOPT_USERNAME, nm->email_from_username);
 		curl_easy_setopt(curl, CURLOPT_PASSWORD, nm->email_from_password);
-
 		curl_easy_setopt(curl, CURLOPT_URL, "smtp://smtp.gmail.com:587");
 		curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
 		curl_easy_setopt(curl, CURLOPT_MAIL_FROM, nm->email_from_username);
-
-
 		for(int i = 0; i< MAX_RECVRS; ++i){ //adds all recipients from std::string recievers[]
 			if(strcmp(nm->recievers[i], "") == 0)break;
 			recipients = curl_slist_append(recipients, nm->recievers[i]);
 		}
-
 		curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
-
 		curl_easy_setopt(curl, CURLOPT_READDATA, FP);
 		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-		//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-		//curl_easy_perform(curl); //the actual email sending is done here
 		if(!is_viable_nm(nm))return -2;
 		res = curl_easy_perform(curl);
-		//curl_easy_cleanup(curl);
 		remove(tmp_file);
-		//return 0;
 		return (int)res;
 	}
 	return -1;
--- c/send.c
+++ c/send.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h> //for getpass()
-#include <dirent.h> //for files in directory
-//#include <vector>
+#include <unistd.h> // for getpass()
 #include "email_notifier.h"
 
 #define LIBB64
@@ -17,29 +15,12 @@ void p_err(int ret){
 	else if(ret == 0)printf("email sent succesfully\n");
       else printf("unknown error: %i occured\n", ret);
 }
-/*
- *
- * //std::string* files_in_dir(std::string directory){
- * std::vector<std::string> files_in_dir(std::string directory){
- *      //std::string* dir_files = new std::string[MAX_ATTACHMENT_NUM];
- *      std::vector<std::string> dir_files;
- *      DIR* dir = opendir(directory.c_str());
- *      dirent* pdir;
- *      int c = 0; 	
- *      while((pdir = readdir(dir))){ //in paren to silence warning
- *            if(strncmp(pdir->d_name, ".", 2) != 0 && strncmp(pdir->d_name, "..", 2) != 0)dir_files.push_back(pdir->d_name);
- *            //dir_files[c++] = pdir->d_name;
- *      }
- *      return dir_files;
- *}
- *
- */
 
+// TODO: fix ridiculous memory leaks
 int main(int argc, char* argv[]){
 	struct notification_message* nm = init_nm();
 	bool atch_sp = false, reciever_sp = false, sub_sp = false, msg_sp = false; //sp - specified
 	if(argc > 1){
-		//std::string flag = "";
             char flag[2];
 		int rc = 0; int at = 0; bool snd = false; 
 		for(int i = 0; i < argc; ++i){
@@ -65,32 +46,18 @@ int main(int argc, char* argv[]){
                         memset(auth_filename, '\0', strlen(auth_filename));
                         strcpy(auth_filename, argv[i+1]);
                   }
-			if(strcmp(flag, "-S") == 0)snd = true; //mail should be sent immediately with no further input
+			if(strcmp(flag, "-S") == 0)snd = true; // mail should be sent immediately with no further input
 			if(strcmp(flag, "-u") == 0)strcpy(nm->email_from_username, argv[i+1]);
 			if(strcmp(flag, "-p") == 0)strcpy(nm->email_from_password, argv[i+1]);
-                  /*
-			 *if(strcmp(flag, "-R") == 0){
-			 *      std::vector<std::string> dir = files_in_dir(argv[i+1]);
-			 *      for(int j=0; j <dir.size(); ++j){
-			 *            std::string fname(argv[i+1]);// uses both dir and filenames
-			 *            fname += ("/" + dir[j]); // to get full directory
-			 *            nm->attachments[at++] = fname;
-			 *      }
-			 *}
-                   */
 			if(strcmp(flag, "-h") == 0){
-				//std::cout << 
                         printf(
-				"usage:\n-s subject\n-m message\n-r reciever\n-a attachment\n-A auth_filename (if none specified, auth.txt will be used)"
-				"\n-S send with no further input\n-u username\n-p password\n-R recursively attach directory\n-h display this help\n\n1-%i recievers can be specified\n1-%i attachments can be specified", MAX_RECVRS, MAX_ATTACHMENT_NUM);
-                        /*
-				 *std::cout << "\n1-" << MAX_RECVRS << " recievers can be specified" << std::endl;
-				 *std::cout << "1-" << MAX_ATTACHMENT_NUM << " attachments can be specified" << std::endl;
-                         */
+				"usage:\n-s subject\n-m message\n-r reciever\n-a attachment\n-A auth_filename \
+                        (if none specified, auth.txt will be used)\n-S send with no further input\n-u \
+                        username\n-p password\n-R recursively attach directory\n-h display this help\n\
+                        \n1-%i recievers can be specified\n1-%i attachments can be specified",
+                        MAX_RECVRS, MAX_ATTACHMENT_NUM);
 				return 0;
 			}
-
-			//flag = "";
 		}
 		if(snd){
 			bool use_auth_file = (strcmp(nm->email_from_username, "") == 0 || strcmp(nm->email_from_password, "") == 0);
@@ -98,11 +65,6 @@ int main(int argc, char* argv[]){
 				char** auth_info = get_auth_info();
                         strcpy(nm->email_from_username, auth_info[0]);
                         strcpy(nm->email_from_password, auth_info[1]);
-                        /*
-				 *nm->email_from_username = auth_info[0];
-				 *nm->email_from_password = auth_info[1];
-                         */
-				//delete[] auth_info;
                         free(auth_info); // lol
 			}
 			int ret = notify(nm);
@@ -112,12 +74,9 @@ int main(int argc, char* argv[]){
 	}
 	if((strcmp(auth_filename, "") == 0 || !file_exists(auth_filename)) && (strcmp(nm->email_from_username, "") == 0 || strcmp(nm->email_from_password, "") == 0)){ // auth filename will never be "", initialized to auth.txt
 		printf("enter username\n");
-		//std::getline(std::cin, nm->email_from_username);
             size_t sz = 0;
             getline(&nm->email_from_username, &sz, stdin);
-
 		printf("\nenter password\n");
-            //sz = 0;
 		nm->email_from_password = getpass("");
 	}
 	if(!reciever_sp){
@@ -125,7 +84,6 @@ int main(int argc, char* argv[]){
 		char* rec_tmp;
             size_t sz = 0;
 		for(int i = 0; i < MAX_RECVRS; ++i){
-			//std::getline(std::cin, rec_tmp);
                   getline(&rec_tmp, &sz, stdin);
 			if(strcmp(rec_tmp, "\n") == 0)break;
 			nm->recievers[i] = rec_tmp;
@@ -134,28 +92,22 @@ int main(int argc, char* argv[]){
 
 	if(!sub_sp){
 		printf("enter email subject\n");
-		//std::getline(std::cin, nm->subject);
             size_t sz = 0;
             getline(&nm->subject, &sz, stdin);
 	}
 
 	if(!msg_sp){
 		printf("enter email message\n");
-		//std::getline(std::cin, nm->message);
             size_t sz = 0;
             getline(&nm->message, &sz, stdin);
 	}
 		
 	if(!atch_sp){
-	//TODO: 
-	//count size. if size >= no more attachments can be attached
 		printf("enter attachments to send\n");
-		//std::string atch = "";
             char* atch;
             size_t sz = 0;
             ssize_t read = 0;
 		for(int i = 0; i < MAX_ATTACHMENT_NUM; ++i){
-			//std::getline(std::cin, atch);
                   read = getline(&atch, &sz, stdin);
                   if(atch[read-1] == '\n')atch[read-1] = '\0';
 			if(strcmp(atch, "") == 0)break;
@@ -164,7 +116,6 @@ int main(int argc, char* argv[]){
 				printf("File will be added to upload list\n");
 				printf("\ncurrent upload list is: \n");
 				for(int j = 0; j <= i; ++j){
-					//std::cout << nm->attachments[j] << " : " << filesize(nm->attachments[j]) << " MB " <<  std::endl;
                               printf("%s : %i MB\n", nm->attachments[j], filesize(nm->attachments[j]));
 				}
 			}
@@ -178,11 +129,9 @@ int main(int argc, char* argv[]){
 	while(getchar() != '\n');
 	bool use_auth_file = (strcmp(nm->email_from_username, "") == 0 || strcmp(nm->email_from_password, "") == 0);
 	if(file_exists(auth_filename) && use_auth_file){
-		//std::string* auth_info = get_auth_info();
             char** auth_info = get_auth_info();
 		nm->email_from_username = auth_info[0];
 		nm->email_from_password = auth_info[1];
-		/*delete[] auth_info;*/
 	}
 	int ret = notify(nm);
       p_err(ret);
